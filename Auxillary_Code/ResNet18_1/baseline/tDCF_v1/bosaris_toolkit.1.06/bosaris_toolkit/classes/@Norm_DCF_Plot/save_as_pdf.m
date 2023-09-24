a49814ae5a4306b2e8b06e9c8bdbcec771b705b5
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2023-09-24 17:18:22 +0200

    update

--- Auxillary_Code/ResNet18_1/baseline/tDCF_v1/bosaris_toolkit.1.06/bosaris_toolkit/classes/@Norm_DCF_Plot/save_as_pdf.m
+++
@@ -1,9 +0,0 @@
-function save_as_pdf(plot_obj,outfilename)
-% Saves the plot to a PDF file.  
-% Inputs:
-%   outfilename: The name of the PDF file in which the plot will be
-%     saved. 
-
-assert(ischar(outfilename))
-saveas(plot_obj.fh,outfilename,'pdf');
-end
